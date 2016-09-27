8f20252d5346c486351f7cc9d2d8c5f8d913318c
Alexander Günsche <mail@lxg.de>
2016-09-27 11:09:40 +0200

    allow public access to generic getter

--- lib/Service/ProviderService.php
+++ lib/Service/ProviderService.php
@@ -27,60 +27,60 @@ class ProviderService
         LanguageRepository $languageRepository,
         TimezoneRepository $timezoneRepository)
     {
-        $this->repositories['Currency'] = $currencyRepository;
-        $this->repositories['Country'] = $countryRepository;
-        $this->repositories['Language'] = $languageRepository;
-        $this->repositories['Timezone'] = $timezoneRepository;
+        $this->repositories["Currency"] = $currencyRepository;
+        $this->repositories["Country"] = $countryRepository;
+        $this->repositories["Language"] = $languageRepository;
+        $this->repositories["Timezone"] = $timezoneRepository;
     }
 
     public function getCountries(array $list = null)
     {
-        return $this->getList('Country', $list);
+        return $this->getList("Country", $list);
     }
 
     public function getCountry($id)
     {
-        return $this->getOne('Country', $id);
+        return $this->getOne("Country", $id);
     }
 
     public function getCurrencies(array $list = null)
     {
-        return $this->getList('Currency', $list);
+        return $this->getList("Currency", $list);
     }
 
     public function getCurrency($id)
     {
-        return $this->getOne('Currency', $id);
+        return $this->getOne("Currency", $id);
     }
 
     public function getLanguages(array $list = null)
     {
-        return $this->getList('Language', $list);
+        return $this->getList("Language", $list);
     }
 
     public function getLanguage($id)
     {
-        return $this->getOne('Language', $id);
+        return $this->getOne("Language", $id);
     }
 
     public function getTimezones(array $list = null)
     {
-        return $this->getList('Timezone', $list);
+        return $this->getList("Timezone", $list);
     }
 
     public function getTimezone($id)
     {
-        return $this->getOne('Timezone', $id);
+        return $this->getOne("Timezone", $id);
     }
 
-    private function getList($entityName, array $list = null)
+    public function getList($entityName, array $list = null)
     {
         return ($list === null)
             ? $this->repositories[$entityName]->findAll()
-            : $this->repositories[$entityName]->findBy(['id' => $list]);
+            : $this->repositories[$entityName]->findBy(["id" => $list]);
     }
 
-    private function getOne($entityName, $id)
+    public function getOne($entityName, $id)
     {
         return $this->repositories[$entityName]->find($id);
     }
